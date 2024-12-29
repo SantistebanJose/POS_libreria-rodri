46f376b04660e6669711339eb9425ad1bac3cceb
frvf2000 <103892870+frvf2000@users.noreply.github.com>
2024-12-29 00:34:44 -0500

    ploteo de mrd
    pacho vamos a tomar

---
+++ ploteo.php
@@ -0,0 +1,21 @@
+<?php
+include("cabecera.php");
+?>
+
+<div
+    class="container">
+
+    <div class="page-inner">
+        <div class="card text-start">
+
+            <div class="card-body">
+                <h4 class="card-title">Ploteo</h4>
+                <p class="card-text">Pachooooooooooooooo</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("pie.php");
+?>
