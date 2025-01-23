68f0e5277ed34c6cb8d05d3886a828eab7aa42cd
frvf2000 <103892870+frvf2000@users.noreply.github.com>
2025-01-23 15:02:29 -0500

    modificacion
    adasd

--- logica/clssVentaCorte.php
+++ logica/clssVentaCorte.php
@@ -1,16 +1,17 @@
 <?php
 include("bd.php");
 
-if (isset($_POST["accion"])){
+if (isset($_POST["accion"])) {
     $accion = $_POST["accion"];
     controladorVentaCorte($accion);
 }
 
-function controladorVentaCorte($accion){
-    switch($accion){
+function controladorVentaCorte($accion)
+{
+    switch ($accion) {
         case 'CONSULTARRESERVA':
             $venta_id = $_POST["venta_id"];
-           
+
             consultarDetalleReserva($venta_id);
             break;
         case 'REGISTRARRESERVA':
@@ -19,33 +20,38 @@ function controladorVentaCorte($accion){
             break;
         case 'CAMBIARCONTRASEÑA':
             break;
-
     }
 }
 
-function consultarDetalleReserva($venta_id){
+function consultarDetalleReserva($venta_id)
+{
     global $conectar;
 
-    try{
-        $orden = $conectar ->prepare("SELECT 
-                rva.id AS rel_venta_articulo_id,
-                rva.venta_id,
-                rva.articulo_id,
-                CONCAT(ar.nombre, ' (', dim.medida, ')') AS articulo_nombre,
-                rva.cantidad,
-                rva.precio_unitario_articulo,
-                rva.minutos,
-                rva.costo_por_minuto,
-                rva.sub_total,
-                ar.corte
-            FROM 
-                rel_venta_articulo AS rva
-            INNER JOIN 
-                articulo AS ar 
-                ON rva.articulo_id = ar.id
-            LEFT  JOIN 
-                dimension AS dim 
-                ON ar.dimension_id = dim.id
+    try {
+        $orden = $conectar->prepare("
+        SELECT 
+        rva.id AS rel_venta_articulo_id,
+        rva.venta_id,
+        rva.articulo_id,
+        CASE 
+            WHEN ar.dimension_id IS NOT NULL THEN
+                CONCAT(ar.nombre, ' (', dim.medida, ')')
+            WHEN ar.nombre IS NULL THEN
+                m.descripcion
+            ELSE
+                ar.nombre 
+        END as articulo_nombre,
+        rva.cantidad,
+        rva.precio_unitario_articulo,
+        rva.minutos,
+        rva.costo_por_minuto,
+        rva.sub_total,
+        ar.corte,
+        rva.movimiento_id
+        FROM rel_venta_articulo AS rva
+        JOIN movimiento as m ON rva.movimiento_id=m.id
+        LEFT JOIN articulo AS ar ON rva.articulo_id = ar.id
+        LEFT  JOIN dimension AS dim ON ar.dimension_id = dim.id
             WHERE 
                 rva.venta_id = :id;");
         $orden->bindParam(":id", $venta_id);
@@ -53,7 +59,6 @@ function consultarDetalleReserva($venta_id){
 
         $lista = $orden->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($lista);
-        
     } catch (\Throwable $th) {
         echo json_encode(["error" => $th->getMessage()]);
     }
